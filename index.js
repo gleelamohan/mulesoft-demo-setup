b2a47674258d973dfe6cb4cf7bd2bcc87ba8f0cc
Leela Mohan <lmohan@salesforce.com>
2020-05-16 20:03:22 +0530

    checkin postscript code

--- index.js
+++ index.js
@@ -4,17 +4,64 @@ const path = require("path");
 const { Pool } = require("pg");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
+const connString =
+	"postgres://gmlmfnycwavomr:4930eaf9b6cd056502799ba0f31b34b557b3c6b9a261bba95dfd945e89f865b7@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d6vd1g4pgmuef0";
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: process.env.DATABASE_URL || connString,
 	ssl: {
 		rejectUnauthorized: false,
 	},
-	user: process.env.User,
-	host: process.env.Host,
-	database: process.env.Database,
-	password: process.env.Password,
-	port: process.env.PORT,
 });
+
+pool.connect();
+
+const query = `
+				CREATE TABLE products (
+						id int,
+						name varchar,
+						quantity int,
+						price int
+				);
+`;
+const checkTableExists = `SELECT
+*
+FROM
+pg_catalog.pg_tables where tablename = 'products'`;
+
+const queryInsert = `
+INSERT INTO products (id, name, quantity, price)
+VALUES (1, 'Paper Bags', 10, 150);
+INSERT INTO products (id, name, quantity, price)
+VALUES (2, 'Air Filter', 100, 169);
+INSERT INTO products (id, name, quantity, price)
+VALUES (3, 'Exhaust Fan', 10, 1000);
+INSERT INTO products (id, name, quantity, price)
+VALUES (4, 'on Site Technician', 100, 169);
+INSERT INTO products (id, name, quantity, price)
+VALUES (5, 'Motor 1', 10, 50);
+INSERT INTO products (id, name, quantity, price)
+VALUES (6, 'Suite', 10, 269);`;
+
+pool
+	.query(checkTableExists)
+	.then((res) => {
+		if (res.rows.length == 0) {
+			pool.query(query).then((res) => {
+				console.log("Table is successfully created");
+				pool.query(queryInsert).then((res) => {
+					console.log("Data insert successful");
+				});
+			});
+		} else {
+			console.log("Table already Exists!!!");
+		}
+	})
+	.catch((err) => {
+		console.error(err);
+	})
+	.finally(() => {
+		//pool.end();
+	});
 
 var router = express.Router();
 
@@ -28,17 +75,8 @@ app.get("/", function (req, res) {
 	res.sendFile(Path + "index.html");
 });
 
-app.get("/configure", function (req, res) {
-	pool.connect();
-
-	const query = `
-				CREATE TABLE users (
-						email varchar,
-						firstName varchar,
-						lastName varchar,
-						age int
-				);
-`;
+/*app.get("/configure", function (req, res) {
+	
 
 	pool.query(query, (err, res1) => {
 		const query1 = `
@@ -58,7 +96,7 @@ app.get("/configure", function (req, res) {
 			return res.send(res2.rows);
 		});
 	});
-});
+}); */
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
