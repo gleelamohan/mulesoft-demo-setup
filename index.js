d2e79efb5180fe5a34161df47450d656293f934d
Leela Mohan <lmohan@salesforce.com>
2020-05-16 15:15:20 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
+const express = require("express");
+const path = require("path");
+const { Pool } = require("pg");
+const PORT = process.env.PORT || 5000;
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
+});
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(express.static(path.join(__dirname, "public")))
+	.set("views", path.join(__dirname, "views"))
+	.set("view engine", "ejs")
+	.get("/", (req, res) => res.render("pages/index"))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
