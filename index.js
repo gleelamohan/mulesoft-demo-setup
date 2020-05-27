a2af2695d5d112c766f43bf6473d667526642b5d
Leela Mohan <lmohan@salesforce.com>
2020-05-27 11:08:50 +0530

    Typo Product Table

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 pool.connect();
 
 const query = `
-				CREATE TABLE products (
+				CREATE TABLE product (
 						id int,
 						name varchar,
 						quantity int,
@@ -26,20 +26,20 @@ const query = `
 const checkTableExists = `SELECT
 *
 FROM
-pg_catalog.pg_tables where tablename = 'products'`;
+pg_catalog.pg_tables where tablename = 'product'`;
 
 const queryInsert = `
-INSERT INTO products (id, name, quantity, price)
+INSERT INTO product (id, name, quantity, price)
 VALUES (1, 'Paper Bags', 10, 150);
-INSERT INTO products (id, name, quantity, price)
+INSERT INTO product (id, name, quantity, price)
 VALUES (2, 'Air Filter', 100, 169);
-INSERT INTO products (id, name, quantity, price)
+INSERT INTO product (id, name, quantity, price)
 VALUES (3, 'Exhaust Fan', 10, 1000);
-INSERT INTO products (id, name, quantity, price)
+INSERT INTO product (id, name, quantity, price)
 VALUES (4, 'on Site Technician', 100, 169);
-INSERT INTO products (id, name, quantity, price)
+INSERT INTO product (id, name, quantity, price)
 VALUES (5, 'Motor 1', 10, 50);
-INSERT INTO products (id, name, quantity, price)
+INSERT INTO product (id, name, quantity, price)
 VALUES (6, 'Suite', 10, 269);`;
 
 pool
