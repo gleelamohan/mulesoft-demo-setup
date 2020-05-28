fa237940a1099af1776371c404e0b4242416f0b5
Leela Mohan <lmohan@salesforce.com>
2020-05-28 19:42:07 +0530

    Commit back to original code

--- index.js
+++ index.js
@@ -1,103 +1,89 @@
-const express = require("express");
-var app = express();
-const path = require("path");
 const { Pool } = require("pg");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
-
-app.get("/api/:id", function (req, res) {
-	var Id = req.params.id;
-	console.log(Id);
-	res.setHeader("Content-Type", "application/json");
-	res.send(
-		JSON.stringify({
-			type: "siren",
-			siren: "428116065",
-			deno: "SOCIETE",
-			greffe: "Paris B 428116065",
-			enseigne: "SOCIETE",
-			psiret: "42811606500049",
-			adresse: "5 RUE DE LA TERRASSE",
-			codepostal: "75017",
-			normcommune: "PARIS",
-			commune: "PARIS",
-			ape: "6312Z",
-			apetexte: "Portails Internet",
-			dateimma: "26-11-1999",
-			dcren: "01-11-1999",
-			nationalite: "France",
-			formejur: "Société par actions simplifiée",
-			capital: {
-				devisecap: "EURO",
-				typecap: "social",
-				capital: "76000.00",
-			},
-		})
-	);
+const connString =
+	"postgres://fdqsjkrhiyzoxn:0547fb5f7f821ec43144dcfc5bc10d51b0fed0c9a037ba7b9faa12f24f333195@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d1f54anjp3c431";
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL || connString,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
-app.get("/:id", function (req, res) {
-	var Id = req.params.id;
-	console.log(Id);
-	res.setHeader("Content-Type", "application/json");
-	res.send(
-		JSON.stringify({
-			type: "siren",
-			siren: "428116065",
-			deno: "SOCIETE",
-			greffe: "Paris B 428116065",
-			enseigne: "SOCIETE",
-			psiret: "42811606500049",
-			adresse: "5 RUE DE LA TERRASSE",
-			codepostal: "75017",
-			normcommune: "PARIS",
-			commune: "PARIS",
-			ape: "6312Z",
-			apetexte: "Portails Internet",
-			dateimma: "26-11-1999",
-			dcren: "01-11-1999",
-			nationalite: "France",
-			formejur: "Société par actions simplifiée",
-			capital: {
-				devisecap: "EURO",
-				typecap: "social",
-				capital: "76000.00",
-			},
-		})
-	);
+pool.connect();
+
+const query = `
+				CREATE TABLE product (
+						id int,
+						name varchar,
+						quantity int,
+						price int
+				);
+`;
+const checkTableExists = `SELECT
+*
+FROM
+pg_catalog.pg_tables where tablename = 'product'`;
+
+const queryInsert = `
+INSERT INTO product (id, name, quantity, price)
+VALUES (1, 'Paper Bags', 10, 150);
+INSERT INTO product (id, name, quantity, price)
+VALUES (2, 'Air Filter', 100, 169);
+INSERT INTO product (id, name, quantity, price)
+VALUES (3, 'Exhaust Fan', 10, 1000);
+INSERT INTO product (id, name, quantity, price)
+VALUES (4, 'on Site Technician', 100, 169);
+INSERT INTO product (id, name, quantity, price)
+VALUES (5, 'Motor 1', 10, 50);
+INSERT INTO product (id, name, quantity, price)
+VALUES (6, 'Suite', 10, 269);`;
+
+pool
+	.query(checkTableExists)
+	.then((res) => {
+		if (res.rows.length == 0) {
+			pool.query(query).then((res) => {
+				console.log("Table is successfully created");
+				pool.query(queryInsert).then((res) => {
+					console.log("Data insert successful");
+				});
+			});
+		} else {
+			console.log("Table already Exists!!!");
+		}
+	})
+	.catch((err) => {
+		console.error(err);
+	})
+	.finally(() => {
+		//pool.end();
+	});
+
+var router = express.Router();
+
+router.use(function (req, res, next) {
+	console.log("/" + req.method);
+	next();
+});
+const Path = __dirname + "/views/";
+
+app.get("/dataurl", function (req, res) {
+	var test = process.env.DATABASE_URL || "NO URI Found";
+	res.send(test);
 });
 
 app.get("/", function (req, res) {
-	res.setHeader("Content-Type", "application/json");
-	res.send(
-		JSON.stringify({
-			type: "siren",
-			siren: "428116065",
-			deno: "SOCIETE",
-			greffe: "Paris B 428116065",
-			enseigne: "SOCIETE",
-			psiret: "42811606500049",
-			adresse: "5 RUE DE LA TERRASSE",
-			codepostal: "75017",
-			normcommune: "PARIS",
-			commune: "PARIS",
-			ape: "6312Z",
-			apetexte: "Portails Internet",
-			dateimma: "26-11-1999",
-			dcren: "01-11-1999",
-			nationalite: "France",
-			formejur: "Société par actions simplifiée",
-			capital: {
-				devisecap: "EURO",
-				typecap: "social",
-				capital: "76000.00",
-			},
-		})
-	);
+	res.sendFile(Path + "index.html");
 });
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", router);
+app.use("*", function (req, res) {
+	res.sendFile(path + "404.html");
+});
 
 app.listen(PORT, function () {
 	console.log(`Listening on ${PORT}`);
